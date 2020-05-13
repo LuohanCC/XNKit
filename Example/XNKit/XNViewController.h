6b86efa6f74b79921596bceb94088904812a368d
jarvis <jarvis.luo@anker.com>
2020-05-13 12:00:33 +0800

    将XNViewController移动到Example中

--- Example/XNKit/XNViewController.h
+++ Example/XNKit/XNViewController.h
@@ -1,13 +1,15 @@
 //
 //  XNViewController.h
-//  XNKit
+//  XNTools
 //
-//  Created by LuohanCC on 12/25/2019.
-//  Copyright (c) 2019 LuohanCC. All rights reserved.
+//  Created by 罗函 on 16/5/23.
+//  Copyright © 2016年 罗函. All rights reserved.
 //
 
-@import UIKit;
+#import <UIKit/UIKit.h>
 
 @interface XNViewController : UIViewController
-
+- (void)setTransparentNavigationBar:(BOOL)enable;
+- (void)initSubviews;
+- (void)initialization;
 @end
